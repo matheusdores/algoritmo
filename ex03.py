6db603dca0e0730f10a85b3f89ef800b7da1cb57
Matheus Dores <105971237+matheusdores@users.noreply.github.com>
2025-03-20 19:12:19 -0300

    Create ex03.py

---
+++ ex03.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Qual o primeiro número:"))
+numero2 = int(input("Qual o segundo número:"))
+total = numero1 + numero2
+
+if(total>10):
+    print("É MAIOR QUE 10!")
+else(total<10):
+    print("NÃO É MAIOR QUE 10!")
